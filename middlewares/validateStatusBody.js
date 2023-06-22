fb4f3c04e792fef005cf0ab6fcfade63dfbc65cf
Hennadii Bukreiev <bukreiev@ukr.net>
2023-06-22 09:04:21 +0300

    fixed validation

--- middlewares/validateStatusBody.js
+++ middlewares/validateStatusBody.js
@@ -1,15 +1,16 @@
 const { httpError } = require('../helpers');
 
-const validateBody = schema => {
+const validateStatusBody = schema => {
   const func = (req, res, next) => {
     const { error } = schema.validate(req.body);
     if (error) {
       if (
         Object.keys(req.body).length === 0 ||
-        !Object.hasOwn(req.body, 'favorite') ||
-        typeof req.body.favorite !== 'boolean'
+        !Object.hasOwn(req.body, 'favorite')
       ) {
         next(httpError(400, `missing field favorite`));
+      } else if (typeof req.body.favorite !== 'boolean') {
+        next(httpError(400, `field favorite has mistakes`));
       }
     }
     next();
@@ -17,4 +18,4 @@ const validateBody = schema => {
   return func;
 };
 
-module.exports = validateBody;
+module.exports = validateStatusBody;
